142377aa4aed2dbcfc64749d8056619798ddb675
ian-flores <iflores.siaca@hey.com>
2022-03-29 06:58:06 -0400

    Adding S3 buckets with pulumi

---
+++ infrastructure/__main__.py
@@ -0,0 +1,21 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import s3
+
+config = pulumi.Config()
+
+project_tags = {
+    "project": pulumi.get_project(),
+    "owner": config.require("owner")
+}
+
+##### S3 Buckets #####
+
+# Define buckets for the project.
+data_bucket = s3.define_s3_data_bucket(project_tags)
+targets_bucket = s3.define_s3_targets_bucket(project_tags)
+
+# Export the name of the buckets.
+pulumi.export('data_bucket_name', data_bucket.id)
+pulumi.export('targets_bucket_name', targets_bucket.id)
